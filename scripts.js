6133bba80846569b4a10221d381ac4b5c48108c8
Gustavo Gantois <gugagantois@gmail.com>
2025-04-18 00:17:52 -0300

    feat:Create modal form.

--- scripts.js
+++ scripts.js
@@ -6,3 +6,28 @@ menuBtn.addEventListener("click", () => {
     navbarContainer.classList.toggle("active")
     
 })
+
+/*Form modal*/
+const modalFormBtn = document.querySelector("#modal-form-btn");
+const modal = document.querySelector("#form-modal-container");
+const closeModal = document.querySelector("#close-modal");
+const overlay = document.querySelector("#modal-overlay");
+const sendBtnForm = document.querySelector("#button-submit");
+const paragraph = document.querySelector(".paragraph")
+
+modalFormBtn.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+
+closeModal.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+    modal.classList.add("active");
+    modal.classList.remove("active")
+})
+
+sendBtnForm.addEventListener("click", () => {
+    alert("Esse formulário não vai pra lugar nenhum kkkk...")
+})
